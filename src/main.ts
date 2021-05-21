2792430798fc08dceac49278d259865e9f6f477a
Ben Hermann <ben.hermann@googlemail.com>
2021-05-21 16:26:06 +0200

    Adapted to new use case

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { context } from '@actions/github/lib/utils';
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+
+    // Only work on issue comments.
+    if (github.context.eventName != "issue_comment")  {
+      core.error("This is action is only valid on issue comments events.");
+      return;
+    }
+
+    // Check for volunteer message 
+    if (context.payload.comment!.body.toLowerCase().includes("I would like to work on this please!")) {
+      core.debug("Found volunteer message.");
+      
+    }
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
@@ -10,6 +25,7 @@ async function run(): Promise<void> {
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
+    
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
