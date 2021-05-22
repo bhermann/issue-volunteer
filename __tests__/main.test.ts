ef6f4c5035adb0cef8588d76af22693e6484adb0
Ben Hermann <ben.hermann@googlemail.com>
2021-05-22 11:32:40 +0200

    Deactivated old tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,11 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
+test('nothing to see here', async() => {
+  
+})
+
+/*
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
@@ -26,3 +31,4 @@ test('test runs', () => {
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
+*/
